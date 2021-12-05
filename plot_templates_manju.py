d30bac687764363c57bb1d30ee1b5e15998a925c
Msquare <41698531+Matammanjunath@users.noreply.github.com>
2021-12-04 23:06:26 -0500

    Update plot_templates_manju.py
    complete change of the code resulting in fewer lines. This code generates any number of y plots with all having a common x plot. Also, each subplot will have a different color

--- plot_templates_manju.py
+++ plot_templates_manju.py
@@ -5,43 +5,38 @@ Created on Sat Dec  4 20:39:23 2021
 @author: MMatam
 """
 import matplotlib.pyplot as plt
-
-
+#https://matplotlib.org/stable/gallery/color/named_colors.html
+import matplotlib.colors as mcolors
 
 
 
 def oscilloscope_plot(df,x='x',y=['y1','y2'],xlbl = ['xlabel','xuni'],
-                      ylbl = [['ylabel1','yuni1'],['ylabel2','yuni2'],['ylabel3','yuni3']],
+                      ylbl = [['ylabel1','yuni1'],['ylabel2','yuni2']],
                       saveformat='Oscilloscope_plot.jpg'):
-    ax1 = plt.subplot(311, sharex=None)
-    plt.plot(df[x], df[y[0]],'-r')
-    plt.ylabel('%s'%(ylbl[0][0]))
-    ax2 = plt.subplot(312, sharex=ax1)
-    plt.plot(df[x], df[y[1]],'-r')
-    plt.ylabel('%s'%(ylbl[1][0]))
-    ax3 = plt.subplot(313, sharex=ax1)
-    plt.plot(df[x], df[y[2]],'-r')
-    plt.ylabel('%s'%(ylbl[2][0]))
-    # ax1.get_shared_x_axes().join(ax1, ax2, ax3)
-    # make x axis on upper invisible
-    plt.setp(ax1.get_xaxis(), visible=False)
-    plt.setp(ax2.get_xaxis(), visible=False)
-    ax1.spines['bottom'].set_visible(False)
-    # ax1.set_ylim([20,40])
-    ax2.spines['top'].set_visible(False)
-    ax2.spines['bottom'].set_visible(False)
-    # ax2.set_ylim([0,1])
-    ax3.spines['top'].set_visible(False)
-    ax3.spines['bottom'].set_visible(True)
-    ax1.legend('(in %s)'%(ylbl[0][1]))
-    ax2.legend('(in %s)'%(ylbl[1][1]))
-    ax3.legend('(in %s)'%(ylbl[2][1]))
-    # ax3.set_ylim([0,25])
-    ax1.grid(axis="y")
-    ax2.grid(axis="y")
-    ax3.grid(axis="y")
-    ax3.set_xlabel('%s'%(ylbl[1][0]))
+    color_list = list(mcolors.TABLEAU_COLORS)
+    n = len(y)
+    fig, axes = plt.subplots(nrows=n,ncols=1,sharex=True)
+    x = ['Time','S']
+    y = [['POA','W/m$^2$'],['str1vol','V'],['str1cur','A']]
+    for i in range(n):
+        axes[i] = plt.subplot((n*100)+10+(i+1))
+        plt.plot(df.index, df[y[i][0]],'-',color=color_list[i])
+        axes[i].grid(axis="y")
+        axes[i].legend(['%s'%(y[i][0])])
+        plt.ylabel('(in %s)'%(y[i][1]))
+        if i==0:
+            axes[i].spines['bottom'].set_visible(False)
+        elif i!=(n-1):
+            plt.setp(axes[i].get_xaxis(), visible=False)
+            axes[i].spines['top'].set_visible(False)
+            axes[i].spines['bottom'].set_visible(False)
+        else:
+            plt.setp(axes[i].get_xaxis(), visible=True)
+            axes[i].spines['top'].set_visible(False)
+            axes[i].spines['bottom'].set_visible(True)
+            axes[i].set_xlabel('%s (in %s)'%(x[0],x[1]))
     plt.subplots_adjust(hspace=0.01)
     plt.subplots_adjust(left=0.12, right=0.97, top=0.95, bottom=0.15)
-    plt.savefig(saveformat,bbox_inches='tight',pad_inches=0.1, dpi=250)
+    temp_name = 'sun_inverterDCparams.png'
+    plt.savefig(temp_name,bbox_inches='tight',pad_inches=0.1, dpi=250)
     plt.show()
